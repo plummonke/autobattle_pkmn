fbd3c55ad2c9cd7ff4fb2a432777904fa21f2ed9
Tullis, Sean <plummonke@gmail.com>
2026-05-06 22:09:38 -0600

    feat: Add EndpointRequestError

--- src/api/endpoint.py
+++ src/api/endpoint.py
@@ -3,7 +3,8 @@ from typing import Dict
 class Endpoint:
     def __init__(self, url: str, endpoint_data: Dict):
         self.base_url = url
-        self.endpoint = f'{url}/{endpoint_data["name"]}'
+        self.name = endpoint_data['name']
+        self.endpoint = f'{url}/{self.name}'
 
     def get_data(self, id: str | int) -> Dict:
         req = Request(self.format_url(id))
@@ -13,5 +14,8 @@ class Endpoint:
         return f'{self.endpoint}/{id}'
 
     def send_request(self, req: Request) -> bytes:
-        response = urlopen(req)
-        return response.read()
+        try:
+            response = urlopen(req)
+            return response.read()
+        except HTTPError as e:
+            raise EndpointRequestError(self.name, e.code)
--- src/api/exceptions.py
+++ src/api/exceptions.py
@@ -6,3 +6,10 @@ class ServiceInitializationError(Exception):
 
     def __str__(self):
         return self.message
+
+class EndpointRequestError(Exception):
+    def __init__(self, endpoint_name: str, code: int):
+        self.message = f'{endpoint_name} failed with HTTP status code {code}.'
+
+    def __str__(self):
+        return self.message
