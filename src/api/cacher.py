bb188ac6afafca679c5be8042cad3151ee92d341
Tullis, Sean <plummonke@gmail.com>
2026-05-07 07:02:58 -0600

    feat: Update cacher

--- src/api/cacher.py
+++ src/api/cacher.py
@@ -1,6 +1,21 @@
+import json
+
 from datetime import date
+from typing import Dict
 
 class Cacher:
-    def __init__(self):
-        pass
+    def __init__(self, base_filename: str):
+        today = date.today().strfmt('%Y%m%d')
+        self.filename = 'cache/' + base_filename + today + '.json'
 
+        self.create_new_file(filename)
+
+    def create_new_file(self, filename: str):
+        with open(filename, 'x') as f:
+            f.write('{}')
+
+    def write_to_file(self, data: Dict):
+        with open(self.filename, 'wr') as f:
+            file = json.load(f)
+            file.update(Dict)
+            json.dump(file, f)
